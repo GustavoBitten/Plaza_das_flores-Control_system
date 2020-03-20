765a4e08e3eafbfc0ccd041dc1b138e87a2dab8e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-03-20 13:50:35 -0300

    Pequenas refinarias

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ let app = express()
 app.set('view engine', 'ejs');
 
 
-app.use(express.static("public"))
-app.use(express.static("node_modules"))
+app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/node_modules"))
 
 app.use('/', homePageRouter);
-
-
-
 
 
 app.listen(3000, ()=>console.log("Esse servidor tá livre de corona virus"))
