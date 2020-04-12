a9b5db84b8a2cc289e0a266a458ac839ad7b4f2b
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-04-12 17:31:19 -0300

    Página de Areas Comuns do Síndico

--- routes/backoffice/backofficePageRouter.js
+++ routes/backoffice/backofficePageRouter.js
@@ -17,6 +17,7 @@ route.get('/morador/portaria', backofficePageController.moradorPortaria)
 route.get('/sindico/moradores', backofficePageController.sindicoMoradores)
 route.get('/sindico/perfil', backofficePageController.sindicoPerfil)
 route.get('/sindico/ocorrencias', backofficePageController.sindicoOcorrencias)
+route.get('/sindico/areas-comuns', backofficePageController.sindicoAreasComuns)
 route.get('/sindico/portaria', backofficePageController.sindicoPortaria)
 
 //Rotas para portaria
