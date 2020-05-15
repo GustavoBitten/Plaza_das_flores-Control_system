876c1bf6137395a0c3fb68a26418423cf9c8a2f3
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-05-14 22:17:39 -0300

    migration cobranca e notificacoes

---
+++ database/migrations/20200515001116-create-cobrancas.js
@@ -0,0 +1,56 @@
+
+    'use strict';
+
+    module.exports = {
+      up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('cobrancas', { 
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+          },
+          morador_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'moradores',
+              key: 'id'
+            },
+            onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+            onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+          },
+          tipo_cobrancas_id: {
+            type: Sequelize.STRING(100),
+            allowNull: false,
+            references: {
+              model: 'tipo_cobrancas',
+              key: 'id'
+            },
+            onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+            onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+         
+          },
+          valor:{
+            type: Sequelize.FLOT()
+          },
+          codigo:{
+            type: Sequelize.STRING(50),
+            allowNull: false
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false
+          }
+        })
+      },
+    
+      down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('cobrancas')
+      }
+    };
+    
---
+++ database/migrations/20200515001145-create-notificacoes.js
@@ -0,0 +1,70 @@
+
+    'use strict';
+
+    module.exports = {
+      up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('notificacoes', { 
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+          },
+
+          morador_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'moradores',
+              key: 'id'
+            },
+            onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+            onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+          },
+    
+
+          descricao:{
+            type: Sequelize.STRING(),
+            allowNull: false
+          },
+          situacao:{
+            type: Sequelize.STRING()
+          },
+
+
+          tipo_notificacoes_id: {
+            type: Sequelize.STRING(100),
+            allowNull: false,
+            references: {
+              model: 'tipo_notificacoes',
+              key: 'id'
+            },
+            onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+            onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+         
+          },
+          data:{
+            type: Sequelize.DATE(),
+            allowNull: false,
+
+          },
+          
+
+
+
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false
+          }
+        })
+      },
+    
+      down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('cobrancas')
+      }
+    };
+    
