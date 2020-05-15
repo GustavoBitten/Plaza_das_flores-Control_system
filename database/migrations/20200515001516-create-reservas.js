84e8dc47fd0ca8d96a2ea83910d78fbb7ba71642
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-05-14 21:26:40 -0300

    Migration reservas e areas_comuns

---
+++ database/migrations/20200515001516-create-reservas.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('reservas', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      morador_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'moradores',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+        onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+      },
+      area_comun_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'areas_comuns',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+        onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+      },
+      data: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 1,
+        allowNull: false
+      },
+      motivo: {
+        type: Sequelize.STRING(500),
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('reservas')
+  }
+};
