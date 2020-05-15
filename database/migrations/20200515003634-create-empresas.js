d227fa2488959470cd3eb1fd6a8c50ceecf3ad89
DevRafaelCena <rafael.cena@hotmail.com>
2020-05-14 21:52:42 -0300

    Migrations empresas

---
+++ database/migrations/20200515003634-create-empresas.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('empresas', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      morador_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'moradores',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE', // whenever there is an user id alteration, udpate the foreign key too
+        onDelete: 'CASCADE' // if an user gets deleted, delete publications of this user
+      },
+      nome: {
+        type: Sequelize.STRING(192),
+        allowNull: false
+      },
+      cnpj: {
+        type: Sequelize.STRING(15),        
+      },
+      rg: {
+        type: Sequelize.STRING(15),        
+      },
+      foto: {
+        type: Sequelize.STRING,        
+      },
+
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('empresas')
+  }
+};
