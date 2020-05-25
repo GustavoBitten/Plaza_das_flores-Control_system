a35047f4c5f9a5e1be183d07301aac4db07af50e
Lincoln dos Santos <lincolnclemente@gmail.com>
2020-05-25 18:25:29 -0300

    adicinados models Notificacao e Tipo_notificacao (temps)

---
+++ tempModels/Notificacao.js
@@ -0,0 +1,57 @@
+module.exports = (sequelize, DataTypes) => {
+    const Notificacao = sequelize.define(
+      "Notificacao",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          tipo_notificacao_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+          data: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          descricao: {
+            type: DataTypes.STRING(),
+            allowNull: false
+          },
+          situacao: {
+            type: DataTypes.STRING(),
+          },
+          created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+        
+      },
+      {
+        timestamps: true, // utliza
+        tablename: "notificacoes"
+
+      }
+    );
+  
+    Notificacao.associate = (models) => {
+      Notificacao.belongsTo(models.Tipo_notificacao, {
+        foreignKey: "tipo_notificacao_id"
+      });
+  
+    };
+  
+    
+  
+    return Reserva;
+  };
---
+++ tempModels/Tipo_notificacao.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+    const Tipo_notificacao = sequelize.define(
+      "Tipo_notificacao",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        tipo: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+          },
+          created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+        
+      },
+      {
+        timestamps: true,
+        tablename: "tipos_notificacoes"
+
+      }
+    );
+  
+    Tipo_notificacao.associate = (models) => {
+      Tipo_notificacao.hasMany(models.Notificacao, {
+        foreignKey: "tipo_notificacao_id",
+      });
+  
+    };
+  
+    
+  
+    return Tipo_notificacao;
+  };
