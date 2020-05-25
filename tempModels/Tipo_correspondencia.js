d8e65a6467ae2652f4b3e7b911b651db196dbbb1
Lincoln dos Santos <lincolnclemente@gmail.com>
2020-05-25 17:32:09 -0300

    adicinado model tipos_correspondencias

---
+++ tempModels/Tipo_correspondencia.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+    const Tipo_ocorrencia = sequelize.define(
+      "Tipo_correspondencia",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        tipo: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+          },
+          created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+        
+      },
+      {
+        timestamps: true,
+        tablename: "tipos_correspondencias"
+
+      }
+    );
+  
+    Tipo_correspondencia.associate = (models) => {
+      Tipo_correspondencia.hasMany(models.Correspondencia, {
+        foreignKey: "tipo_correspondencia_id",
+      });
+  
+    };
+  
+    
+  
+    return Tipo_correspondencia;
+  };
