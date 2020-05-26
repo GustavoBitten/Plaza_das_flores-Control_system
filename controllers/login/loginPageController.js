8fe0f8d30ac3cd793be477cea499b738260ad3fe
Gustavo Duarte <gustavoduarte1999@hotmail.com>
2020-05-26 00:58:53 -0300

    inicio de auth por senha

--- controllers/login/loginPageController.js
+++ controllers/login/loginPageController.js
@@ -1,3 +1,5 @@
+const express = require('express')
+const app = express() 
 const {Usuario} = require('../../models')
 const session = require('express-session')
 
@@ -9,17 +11,47 @@ const  loginPageController = {
   
   auth: async (req,res) => {
     const {email,senha} = req.body
+
     console.log(email,senha)
     const usuario = await Usuario.findOne({
       where: {email}
     })
 
+    if (usuario == undefined) {
+      res.render("login/login", {titulo:"Acesso"})
+      console.log('passou aqui')
+      let erro = 'deu ruim'
+      app.locals.erro = erro
+    }else{
+
+    }
+
+
+
+
+    usuario.senha = ''
     //console.log(usuario)
-    
     req.session.user = usuario
-    //console.log(req.session.user)
-    res.redirect("backoffice/morador/dashboard")
-  }
+    
+    switch (usuario.tipo_usuario_id) {
+
+      case 1:
+        res.redirect("/backoffice/morador/dashboard")
+        break;
+      case 2:
+        res.redirect("/backoffice/sindico/moradores")
+        break;
+      case 3:
+        res.redirect("/backoffice/portaria/perfil")
+        break;
   
+      default:
+        break;
+    }
+
+    
+  
+}
+
 }
 module.exports = loginPageController
--- utils/ativadorAuth.js
+++ utils/ativadorAuth.js
@@ -1,2 +1,2 @@
 //Escolha True para ativar as autenticações de acesso e False para desativar
-module.exports = true
+module.exports = false
