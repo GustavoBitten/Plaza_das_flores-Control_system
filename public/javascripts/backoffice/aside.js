9230549810ad95be3d35b6783801a9ee669b7f8a
Gustavo Duarte <gustavoduarte1999@hotmail.com>
2020-05-27 23:55:27 -0300

    Otimização do Aside V1

---
+++ public/javascripts/backoffice/aside.js
@@ -0,0 +1,59 @@
+console.log("funcionando")
+
+const asideItens = {
+    morador:[{
+        nome:'Dashboard',
+        href:'dashboard',
+        icon:'fas fa-columns'
+    },{
+        nome:'Perfil',
+        href:'perfil',
+        icon:'fas fa-user-tie'
+    },{
+        nome:'Áreas Comuns',
+        href:'areas-comuns',
+        icon:'fas fa-futbol'
+    },{
+        nome:'Ocorrências',
+        href:'ocorrencias',
+        icon:'fas fa-exclamation'
+    },{
+        nome:'Portaria',
+        href:'portaria',
+        icon:'fas fa-shipping-fast'
+    },{
+        nome:'Comunicados',
+        href:'comunicados',
+        icon:'fas fa-comment-dots'
+    },{
+        nome:'Financeiro',
+        href:'financeiro',
+        icon:'fas fa-dollar-sign'
+    }]
+
+}
+
+
+
+
+
+const url = document.URL
+const typeUrl = url.split('/')[4]
+console.log(typeUrl)
+
+
+const menu = document.getElementById('menu')
+asideItens.morador.forEach(item => {
+    menu.innerHTML += `
+    <a class="nav-link d-flex justify-content-start align-items-center text-white font-weight-bold"
+     href="/backoffice/${typeUrl}/${item.href}">
+    <i class="${item.icon} nav-icon mr-2"></i>
+    <span class="text-link">${item.nome}</span>
+    </a>`   
+
+
+
+
+
+
+});
