4c9e055c4ac626f48e5daa1f1fb79a0358818b01
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-14 01:12:26 -0300

    upgrade aside options for change by typeUser

--- public/javascripts/backoffice/aside.js
+++ public/javascripts/backoffice/aside.js
@@ -1,63 +1,61 @@
-console.log("funcionando")
+const menu = document.getElementById('menu')
 
-const asideItens = {
-    morador:[{
-        nome:'Dashboard',
-        href:'dashboard',
-        icon:'fas fa-columns'
-    },{
-        nome:'Perfil',
-        href:'perfil',
-        icon:'fas fa-user-tie'
-    },{
-        nome:'Áreas Comuns',
-        href:'areas-comuns',
-        icon:'fas fa-futbol'
-    },{
-        nome:'Ocorrências',
-        href:'ocorrencias',
-        icon:'fas fa-exclamation'
-    },{
-        nome:'Correspondências',
-        href:'correspondencias',
-        icon:'fas fa-shipping-fast'
-    },{
-        nome:'Moradores',
-        href:'moradores',
-        icon:'fas fa-users'
-    },{
-        nome:'Comunicados',
-        href:'comunicados',
-        icon:'fas fa-comment-dots'
-    },{
-        nome:'Financeiro',
-        href:'financeiro',
-        icon:'fas fa-dollar-sign'
-    }]
-
+class Item {
+    constructor(name, href, icon) {
+        this.name = name
+        this.href = href
+        this.icon = icon
+    }
 }
 
+const dashboard = new Item('Dashboard', 'dashboard', 'fas fa-columns')
+const perfil = new Item('Perfil', 'perfil', 'fas fa-user-tie')
+const areas_comuns = new Item('Áreas Comuns', 'areas_comuns', 'fas fa-futbol')
+const ocorrencias = new Item('Ocorrências', 'ocorrencias', 'fas fa-exclamation')
+const correspondencias = new Item('Correspondências', 'correspondencias', 'fas fa-shipping-fast')
+const comunicados = new Item('Comunicados', 'comunicados', 'fas fa-comment-dots')
+const financeiro = new Item('Financeiro', 'financeiro', 'fas fa-dollar-sign')
 
+const moradores = new Item('Moradores', 'moradores', 'fas fa-users')
 
+//Para alterar as opções do aside, só alterar os arrays abaixo
+
+const comumItems = [dashboard, perfil, areas_comuns, ocorrencias, correspondencias, comunicados, financeiro]
+
+const moradorItems = [...comumItems]
+const sindicoItems = [...comumItems, moradores]
+const portariaItems = [...comumItems]
 
 
 const url = document.URL
 const typeUrl = url.split('/')[4]
-console.log(typeUrl)
-
-
-const menu = document.getElementById('menu')
-asideItens.morador.forEach(item => {
-    menu.innerHTML += `
-    <a class="nav-link d-flex justify-content-start align-items-center text-white font-weight-bold"
-     href="/backoffice/${typeUrl}/${item.href}">
-    <i class="${item.icon} nav-icon mr-2 iconAside"></i>
-    <span class="text-link">${item.nome}</span>
-    </a>`
 
 
 
+function insertItemsAside(asideItens) {
 
+    asideItens.forEach(item => {
+        menu.innerHTML += `
+        <a class="nav-link d-flex justify-content-start align-items-center text-white font-weight-bold"
+        href="/backoffice/${typeUrl}/${item.href}">
+        <i class="${item.icon} nav-icon mr-2 iconAside"></i>
+        <span class="text-link">${item.name}</span>
+        </a>`
 
+    });
+}
 
-});
+switch (typeUrl) {
+    case 'morador':
+        insertItemsAside(moradorItems)
+        break;
+    case 'sindico':
+        insertItemsAside(sindicoItems)
+        break;
+    case 'portaria':
+        insertItemsAside(portariaItems)
+        break;
+
+    default:
+        break;
+}
